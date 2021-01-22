b269cbcf1d96f99e4bd56eefdd0aa0daed1782d4
zhoucumt <553817478@qq.com>
2021-01-22 11:31:58 +0800

    ref用法‘

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="hello">
     HelloWorld
+    <div>{{n}}</div>
+     <button @click="addN">点击</button>
   </div>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
   name: 'HelloWorld',
+
+  setup() {
+    const n = ref(1);
+    function addN() {
+      n.value++;
+    }
+    return {
+      n,
+      addN
+    }
+  },
+
   props: {
     msg: String
   }
