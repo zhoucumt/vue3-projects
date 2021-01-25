680b1f7c55ade19e8ca4767b82a6677547118dbc
zhoucumt <553817478@qq.com>
2021-01-25 18:05:04 +0800

    ref computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="hello">
-    HelloWorld
+    HelloWorld {{count}}
+    <div>{{plusOne}}</div>
   </div>
 </template>
 
 <script>
-import {ref, reactive, watchEffect} from 'vue';
+import {ref, reactive, computed} from 'vue';
 export default {
   name: 'HelloWorld',
 
   setup() {
-    const state = reactive({
-      name: 'Jack',
-      age: 1,
-      address: 'aaa'
-    });
-    watchEffect(() => {
-      console.log('123');
-    })
+    const count = ref(1)
+    const plusOne = computed(() => count.value + 1)
+
+    console.log(plusOne.value) // 2
+    console.log(count)
+    // console.log(plusOne.value++)
+    plusOne.value++ // error
+    return {
+      count,
+      plusOne
+    }
   },
 
   props: {
